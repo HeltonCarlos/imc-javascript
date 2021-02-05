46ea61fd9c4467bbfafdb49845c4d0744670593f
HeltonCarlos <heltonbritopa@gmail.com>
2021-02-05 20:46:48 -0300

    javascript README
    JAVAAPP

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 document.querySelector('#calcular').addEventListener('click',e=>{
-    alert('teste')
+  var peso =document.querySelector('#peso').value
+  var altura=document.querySelector('#altura').value
+  var resposta=document.querySelector('#resposta')
+
+  if(!peso || !altura){
+      alert('Digite as informações!')
+  }else{
+
+    resposta.innerHTML+=`seu peso é ${peso}, e sua altura é ${altura}`;
+  }
 })
